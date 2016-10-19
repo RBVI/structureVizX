fa48357ea86db4b574e8bdc6b5de2270d1b01cd7
Scooter Morris <scooter@cgl.ucsf.edu>
2016-10-19 16:04:00 -0700

    Use chimeraManager's model list for "list models" command.

--- src/main/java/edu/ucsf/rbvi/structureVizX/internal/tasks/ListModelsTask.java
+++ src/main/java/edu/ucsf/rbvi/structureVizX/internal/tasks/ListModelsTask.java
@@ -9,6 +9,7 @@ import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.ObservableTask;
 import org.cytoscape.work.TaskMonitor;
 
+import edu.ucsf.rbvi.structureVizX.internal.model.ChimeraManager;
 import edu.ucsf.rbvi.structureVizX.internal.model.ChimeraModel;
 import edu.ucsf.rbvi.structureVizX.internal.model.ChimeraStructuralObject;
 import edu.ucsf.rbvi.structureVizX.internal.model.StructureManager;
@@ -24,10 +25,11 @@ public class ListModelsTask extends AbstractTask implements ObservableTask {
 
 	@Override
 	public void run(TaskMonitor taskMonitor) throws Exception {
+		ChimeraManager chimeraManager = structureManager.getChimeraManager();
 		taskMonitor.setTitle("List Models");
 		chimeraModels = new HashSet<ChimeraModel>();
 
-		for (ChimeraStructuralObject obj: structureManager.getAllChimObjs()) {
+		for (ChimeraModel obj: chimeraManager.getChimeraModels()) {
 			if (obj instanceof ChimeraModel)
 				chimeraModels.add((ChimeraModel)obj);
 		}
